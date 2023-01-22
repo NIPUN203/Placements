df9d8d032827767c32254a832a9fbfd3e0660a7e
Nipun <Nipunrawat2003@gmail.com>
2023-01-22 23:38:37 +0530

    Analyzing and handling unbalanced data

--- main.py
+++ main.py
@@ -4,7 +4,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-card_data=pd.read_csv('C:/Users/nipun/OneDrive/Desktop/Ml-Project/credit_card_data.csv')
+card_data=pd.read_csv('C:/Users/nipun/OneDrive/Desktop/ML_Projects/credit_card_data.csv')
 
 print(card_data.head())
 print(card_data.tail())
+
+card_data.info()
+print(card_data.isnull().sum())
+
+print(card_data['Class'].value_counts())
+
+valid_transac=card_data[card_data.Class == 0]
+fraud_transac=card_data[card_data.Class == 1]
+
+print(valid_transac)
+print(fraud_transac)
+
+print(valid_transac['Amount'].describe())
+print(fraud_transac['Amount'].describe())
+
+print(card_data.groupby('Class').mean())
+
+bal_valid_trans=valid_transac.sample(n=492)
+print(bal_valid_trans)
